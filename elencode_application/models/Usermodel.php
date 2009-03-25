312ec6b6b917045bf9b23c6d567b101000344560
Mic <m@micz.it>
2009-03-25 12:17:09 +0100

    usermodel: added a comment with the wp user levels

--- elencode_application/models/Usermodel.php
+++ elencode_application/models/Usermodel.php
@@ -64,4 +64,17 @@ class User {
       return $this->Role==10;
     }
 }
+
+//    Wordpress User Roles from http://codex.wordpress.org/Roles_and_Capabilities
+//    * User Level 0 converts to Subscriber Role
+//    * User Level 1 converts to Contributor Role
+//    * User Level 2 converts to Author Role
+//    * User Level 3 converts to Author Role
+//    * User Level 4 converts to Author Role
+//    * User Level 5 converts to Editor Role
+//    * User Level 6 converts to Editor Role
+//    * User Level 7 converts to Editor Role
+//    * User Level 8 converts to Administrator Role
+//    * User Level 9 converts to Administrator Role
+//    * User Level 10 converts to Administrator Role 
 ?>
