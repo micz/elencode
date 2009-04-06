12ac25d1d26d89ce225b6cfe4f9a651a0348dd48
mic <m@micz.it>
2009-04-07 00:40:33 +0200

    going on working on a generic obj-array table editing system

--- www/e/js/admin.js
+++ www/e/js/admin.js
@@ -37,7 +37,7 @@ function tb_confirm_mod(id,ajax_url,data)
 function tbobj_edit_value(id)
 {
   var tot=$('#tr'+id+' > td').length;
-  for(i=0;i<tot;i++)
+  for(i=1;i<tot;i++)
   {
     $('#v'+id+'_'+i).hide();
     $('#vm'+id+'_'+i).show();
@@ -49,7 +49,7 @@ function tbobj_edit_value(id)
 function tbobj_cancel_mod(id)
 {
   var tot=$('#tr'+id+' > td').length;
-  for(i=0;i<tot;i++)
+  for(i=1;i<tot;i++)
   {
     $('#v'+id+'_'+i).show();
     $('#vm'+id+'_'+i).hide();
@@ -61,12 +61,115 @@ function tbobj_cancel_mod(id)
 function tbobj_confirm_mod(id,ajax_url,data)
 {
   var tot=$('#tr'+id+' > td').length;
-  for(i=0;i<tot;i++)
+  for(i=1;i<tot;i++)
   {
     $('#vm'+id+'_'+i).hide();
   }
   $('#btnm'+id).hide();
   $('#wm'+id).show();
   $('#w'+id).show();
-  //$.ajax({url:ajax_url,type:'POST',dataType:'script',data:data+'&htmlid='+id});
+  $.ajax({url:ajax_url,type:'POST',dataType:'script',data:data+'&htmlid='+id});
+}
+
+function tbobj_get_mod_data(id)
+{
+  var tot=$('#tr'+id+' > td').length;
+  out=new Array();
+  for(i=1;i<tot;i++)
+  {
+    out.push($('#item_value_'+id+'_'+i).val());
+  }
+  return js_serialize(out);
+}
+
+function js_serialize(mixed_value)
+{
+// Returns a string representation of variable (which can later be unserialized)
+//
+// version: 812.3015
+// discuss at: http://phpjs.org/functions/serialize
+// +   original by: Arpad Ray (mailto:arpad@php.net)
+// +   improved by: Dino
+// +   bugfixed by: Andrej Pavlovic
+// +   bugfixed by: Garagoth
+// %          note: We feel the main purpose of this function should be to ease the transport of data between php & js
+// %          note: Aiming for PHP-compatibility, we have to translate objects to arrays
+// *     example 1: serialize(['Kevin', 'van', 'Zonneveld']);
+// *     returns 1: 'a:3:{i:0;s:5:"Kevin";i:1;s:3:"van";i:2;s:9:"Zonneveld";}'
+// *     example 2: serialize({firstName: 'Kevin', midName: 'van', surName: 'Zonneveld'});
+// *     returns 2: 'a:3:{s:9:"firstName";s:5:"Kevin";s:7:"midName";s:3:"van";s:7:"surName";s:9:"Zonneveld";}'
+var _getType = function( inp ) {
+var type = typeof inp, match;
+var key;
+if (type == 'object' && !inp) {
+return 'null';
+}
+if (type == "object") {
+if (!inp.constructor) {
+return 'object';
+}
+var cons = inp.constructor.toString();
+if (match = cons.match(/(\w+)\(/)) {
+cons = match[1].toLowerCase();
+}
+var types = ["boolean", "number", "string", "array"];
+for (key in types) {
+if (cons == types[key]) {
+type = types[key];
+break;
+}
+}
+}
+return type;
+};
+var type = _getType(mixed_value);
+var val, ktype = '';
+switch (type) {
+case "function":
+val = "";
+break;
+case "undefined":
+val = "N";
+break;
+case "boolean":
+val = "b:" + (mixed_value ? "1" : "0");
+break;
+case "number":
+val = (Math.round(mixed_value) == mixed_value ? "i" : "d") + ":" + mixed_value;
+break;
+case "string":
+val = "s:" + mixed_value.length + ":\"" + mixed_value + "\"";
+break;
+case "array":
+case "object":
+val = "a";
+/*
+if (type == "object") {
+var objname = mixed_value.constructor.toString().match(/(\w+)\(\)/);
+if (objname == undefined) {
+return;
+}
+objname[1] = serialize(objname[1]);
+val = "O" + objname[1].substring(1, objname[1].length - 1);
+}
+*/
+var count = 0;
+var vals = "";
+var okey;
+var key;
+for (key in mixed_value) {
+ktype = _getType(mixed_value[key]);
+if (ktype == "function") {
+continue;
+}
+okey = (key.match(/^[0-9]+$/) ? parseInt(key) : key);
+vals += js_serialize(okey) +
+js_serialize(mixed_value[key]);
+count++;
+}
+val += ":" + count + ":{" + vals + "}";
+break;
+}
+if (type != "object" && type != "array") val += ";";
+return val;
 }
