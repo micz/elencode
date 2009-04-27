92295a53cd2ab2590db3da5f5b647b7901eda5b6
Mic <m@micz.it>
2009-04-27 13:58:09 +0200

    updated the contact mail in the source files header

--- elencode_application/views/admin/sidebar.php
+++ elencode_application/views/admin/sidebar.php
@@ -4,9 +4,9 @@
 * This file is part of Elencode.
 * Elencode is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 * Elencode is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License along with Vanilla; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+* You should have received a copy of the GNU General Public License along with Elencode; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 * The latest source code for Elencode is available at https://github.com/micz/elencode
-* Contact Mic at m [at] micz [dot] it
+* Contact the Dev Team at dev [at] elenbar [dot] it
 *
 */
 ?>
